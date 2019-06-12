a719a1c01778884199ea8f347f41c1ee8e6edb3b
li459461891 <li.yongliang@outlook.com>
2019-06-12 17:11:58 +0800

    Update qsort.py
    Add non recursive function.
    Seems 4ms more than recursive function

--- sort/qsort.py
+++ sort/qsort.py
@@ -1,8 +1,14 @@
 """
- python code much more readable and clean than that in C++ !
- use python3 in leetcode, run time double of the c++.
+python code much more readable and clean than that in C++ !
+use python3 in leetcode, run time double of the c++.
+
+python recursive function
 Runtime: 248 ms, faster than 50.22% of Python3 online submissions for Sort an Array.
 Memory Usage: 19.6 MB, less than 80.57% of Python3 online submissions for Sort an Array.
+
+non recursive function
+Runtime: 288 ms, faster than 38.44% of Python3 online submissions for Sort an Array.
+Memory Usage: 19.6 MB, less than 77.26% of Python3 online submissions for Sort an Array.
 """
 class Solution(object):
     def sortArray(self, nums):
@@ -33,6 +39,22 @@ class Solution(object):
         self._qsort(arr, middle_pos + 1, right)
         return arr
 
+    def _push_back_if_valid(self, range_stack, left, right):
+        if left < right and left >= 0:
+            print([left, right])
+            range_stack.append([left, right])
+    
+    # non recursive function
+    def _qsort_non_recursive(self, arr):
+        range_stack = []
+        self._push_back_if_valid(range_stack, 0, len(arr) - 1)
+        while 0 != len(range_stack):
+            popout_range = range_stack.pop()
+            left = popout_range[0]
+            right = popout_range[1]
+            pivot = self._partition(arr, left, right)
+            self._push_back_if_valid(range_stack, left, pivot - 1)
+            self._push_back_if_valid(range_stack, pivot + 1, right)
 
 # The following code need supported by pytest framework
 def test_qsort():
